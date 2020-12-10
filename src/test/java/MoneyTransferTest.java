e476097e15e31d971e5f17e5559b0cba51e47e12
Ani.Harutyunyan <ani.harutyunyan@idt.am>
2020-12-10 09:22:02 +0400

    Added comments

--- src/test/java/MoneyTransferTest.java
+++ src/test/java/MoneyTransferTest.java
@@ -19,13 +19,14 @@ public class MoneyTransferTest {
 
     @DataProvider()
     public Object[][] dpCountriesAndLevels(){
+        //Countries are received from payment-remit-info
         paymentRemitInfoModel = transferEndpoint.getRemitInfoModel(new HashMap());
         List<String> countries = paymentRemitInfoModel.getCountry();
         String[] serviceArr = {"1","2","3"};
         Object[][] returnCountries = new Object[countries.size()*serviceArr.length][2];
         int t = 0;
         for (int i = 0; i < serviceArr.length; i++) {
-            if(t==105){
+            if(t==countries.size()*serviceArr.length){
                 break;
             }
             for (int j = 0; j <countries.size() ; j++) {
@@ -45,8 +46,11 @@ public class MoneyTransferTest {
         softAssert = new SoftAssert();
     }
 
+
+    ///The data for test is received by DataProvider
     @Test(dataProvider = "dpCountriesAndLevels")
     public void toBankAccount(String country,String serviceLevel){
+        ///Setting request object( model)
         RequestObject requestObject = new RequestObject(
                 country,
                 "Account",
@@ -54,7 +58,11 @@ public class MoneyTransferTest {
                 serviceLevel
         );
         requestObject.requestPrettyPrint();
+
+        ///Receiving response model and setting it to response object
         ResponseObject responseObject = transferEndpoint.getRemittanceChargeRate(requestObject.getRequestModel());
+
+        ///Assertions are done step by step, if any of steps is failed will be run the next test case
         Assert.assertEquals(responseObject.getCode(),200);
         System.out.println("Step1 passed");
         Assert.assertFalse(responseObject.isBeneficiaryFieldsEmpty());
@@ -67,7 +75,6 @@ public class MoneyTransferTest {
         System.out.println("Step5 passed");
         Assert.assertTrue(responseObject.isPointsValid());
         System.out.println("Step6 passed");
-        //softAssert.assertAll();
     }
 
     @Test(dataProvider = "dpCountriesAndLevels")
@@ -92,7 +99,6 @@ public class MoneyTransferTest {
         System.out.println("Step5 passed");
         Assert.assertTrue(responseObject.isPointsValid());
         System.out.println("Step6 passed");
-        //softAssert.assertAll();
     }
 
     @Test(dataProvider = "dpCountriesAndLevels")
